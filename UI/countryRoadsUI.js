950ac82007ff29877cb7221b4adbac87c76c40a2
Alex Elias <aelias36@yahoo.com>
2019-10-19 15:23:32 -0400

    Javascript halfway done for MVP

---
+++ UI/countryRoadsUI.js
@@ -0,0 +1,112 @@
+// var cars = [{
+// 		"measurementId": 1234567890,
+// 		"carId": 12345,
+// 		"color": "green",
+// 		"heading": 270,
+// 		"gasLevel": 0.75,
+// 		"speed": 50,
+// 		"time": "019-10-19T12:30:00.000Z",
+// 		"relayStationId": 67890
+// 	},
+// 	{
+// 		"measurementId": 1234567891,
+// 		"carId": 12346,
+// 		"color": "blue",
+// 		"heading": 90,
+// 		"gasLevel": 0.50,
+// 		"speed": 60,
+// 		"time": "019-10-19T12:31:00.000Z",
+// 		"relayStationId": 67890
+// 	}
+// ];
+
+var relayStations = [
+{
+	"relayStationId": 67890,
+	"locationName": "Alpha",
+	"roadName": "Elm Street"
+}
+]
+
+document.addEventListener('DOMContentLoaded', onLoadCalback, false);
+function onLoadCalback() {
+	let url = 'https://country-roads-256405.appspot.com/cars/now';
+
+	fetch(url)
+	.then(res => res.json())
+	.then((out) => {
+	  console.log('Checkout this JSON! ', out);
+	})
+	.catch(err => { throw err });
+
+	writeRelayStationContainers();
+	writeCarContainers(out);
+}
+
+
+// Generate relayStationContainer DOM elements
+function writeRelayStationContainers() {
+	var mainContainer = document.getElementsByClassName('mainContainer')[0];
+
+	for (var i = 0; i < relayStations.length; i++) {
+		console.log(relayStations[i].locationName);
+		
+		relayStationContainer =  document.createElement('div');
+		relayStationContainer.className = 'relayStationContainer';
+		relayStationHeader = document.createElement('div');
+		relayStationHeader.className = 'relayStationHeader';
+		relayStationContainer.appendChild(relayStationHeader);
+		
+		heading = document.createElement('h2');
+		heading.appendChild(document.createTextNode(relayStations[i].locationName + " Relay Station"));
+		relayStationHeader.appendChild(heading);
+
+		stationDescription = document.createElement('p');
+		stationDescription.className = 'stationDescription';
+		stationDescription.appendChild(document.createTextNode(relayStations[i].roadName));
+		relayStationHeader.appendChild(stationDescription);
+
+		stationId = document.createElement('p');
+		stationId.className = 'stationId';
+		stationId.appendChild(document.createTextNode('RS # ' + relayStations[i].relayStationId));
+		relayStationHeader.appendChild(stationId)
+
+		relayStationContent = document.createElement('div');
+		relayStationContent.className = 'relayStationContent';
+		relayStationContent.id = 'relayStationContent'+relayStations[i].relayStationId;
+		relayStationContainer.appendChild(relayStationContent);
+
+
+		mainContainer.appendChild(relayStationContainer);
+	}
+}
+
+function writeCarContainers(cars) {
+	template = document.getElementById('carContainerTemplate');
+	for (var i = 0; i < cars.length; i++) {
+		carContainer = document.importNode(template.content, true);
+		
+		if (cars[i].color == 'green')
+			colorClass = 'squareGreen';
+		else if (cars[i].color == 'blue')
+			colorClass = 'squareBlue';
+		else if (cars[i].color == 'orange')
+			colorClass = 'squareOrange'
+		else // Yellow
+			colorClass = 'squareYellow'
+		carContainer.querySelector('.squareGreen').className = colorClass;
+
+		description = cars[i].color[0].toUpperCase() + cars[i].color.toLowerCase().slice(1)
+			 + ' Car #' + cars[i].carId;
+		carContainer.querySelector('.carDescription').textContent = description;
+
+		carContainer.getElementById('speed').textContent  = cars[i].speed;
+		carContainer.getElementById('gas').textContent  = cars[i].gasLevel*100 + '%';
+		carContainer.getElementById('heading').textContent  = cars[i].heading;
+		carContainer.querySelector('.carTimestamp').textContent  = cars[i].time;
+		carContainer.querySelector('.measId').textContent = 'Meas # ' + cars[i].measurementId;
+
+		relayStationContent = document.getElementById('relayStationContent'+cars[i].relayStationId);
+		relayStationContent.appendChild(carContainer);
+	}
+}
